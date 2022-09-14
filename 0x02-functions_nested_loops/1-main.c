3eb6855e2ac02304da312a0cdf340bb2736704b6
DrAbstract <topa200357@gmail.com>
2022-09-14 15:57:26 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet - Prints lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	print_alphabet(lowercase);
+	return (0);
+}
